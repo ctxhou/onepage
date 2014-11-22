a82d57720e72f1e1bec30c5300db2da9063354dc
JackHou <vjack070707@gmail.com>
2014-11-22 22:14:06 +0800

    init ghpage

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+
+group :development do
+    gem 'sass'
+    gem 'susy'
+    gem 'compass'
+    gem 'breakpoint'
+end
---
+++ js/jquery.onepage.js
@@ -0,0 +1,48 @@
+;(function($) {
+    $.fn.onepage = function(options) {
+        options = $.extend({
+            'resize': true,
+            'navLink': '.header-choice-link',
+            'sectionSelector': '.page_view'
+        }, options);
+
+        var window_width;
+
+        if (options.resize == true) {
+            $(window).resize(function(){
+                header_height = $('header').outerHeight();
+                $(".height_full").css("height", $(window).height()-header_height);
+                window_width = $(window).with();
+            });
+        }
+        // default; append class
+        $(options.sectionSelector).each(function(i, v){
+            $(this).addClass('height_full');
+            window_width = $(window).width();
+            $(this).css("left", i * window_width);
+        });
+        adjustHeight();
+        defaultSection();
+        $(options.navLink).click(function() {
+            $(options.navLink).removeClass('selected');
+            $(this).addClass('selected');
+            current = $(this);
+            $("#onepage-wrapper").scrollTo($(this).attr("href"), 800);
+
+            return false; //prevent the anchor link
+        });
+
+        function adjustHeight() {
+            var header_height = $('header').outerHeight();
+            $(".height_full").css("height", $(window).height()-header_height);
+        }
+
+        function defaultSection() {
+            // when open the page, hide the content except first section
+            $(options.sectionSelector).first().addClass('current_page');
+            var count = $(options.sectionSelector).length;
+            $(options.sectionSelector).css("width", 100/count+"%");
+            $("#onepage").css("width", 100*count+"%");
+        }
+    };
+})(jQuery);
