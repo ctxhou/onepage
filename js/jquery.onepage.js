dce8699f938910d8ac7e666988876b43b6f3fbb3
JackHou <vjack070707@gmail.com>
2014-11-22 17:05:19 +0800

    select nav

--- js/jquery.onepage.js
+++ js/jquery.onepage.js
@@ -2,27 +2,38 @@
     $.fn.onepage = function(options) {
         options = $.extend({
             'resize': true,
-
+            'navLink': '.header-choice-link',
             'sectionSelector': '.page_view'
         }, options);
-
-        // default action
 
         if (options.resize == true) {
             $(window).resize(function(){
                 header_height = $('header').outerHeight();
-                $(".height_full").css("height", $(window).height()-header_height)
-            })
+                $(".height_full").css("height", $(window).height()-header_height);
+            });
         }
+
+        $(options.navLink).click(function() {
+            var href = $(this).attr("href");
+            $(".height_full").css("display", "none");
+            $(href).css("display", "block");
+        });
 
         $(options.sectionSelector).each(function(){
             $(this).addClass('height_full');
             adjustHeight();
+            hideOtherSection();
         });
 
         function adjustHeight() {
             var header_height = $('header').outerHeight();
-            $(".height_full").css("height", $(window).height()-header_height)
+            $(".height_full").css("height", $(window).height()-header_height);
+        }
+
+        function hideOtherSection() {
+            // when open the page, hide the content except first section
+            $(".height_full").css("display", "none");
+            $(".height_full").first().css("display", "block");
         }
     };
 })(jQuery);
